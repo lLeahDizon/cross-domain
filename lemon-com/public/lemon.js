4a424422e45d7f6b67cec60bf24d684505d54cf0
Lemon <516330797@qq.com>
2020-10-08 22:59:27 +0800

    JSONP优化

--- lemon-com/public/lemon.js
+++ lemon-com/public/lemon.js
@@ -1,6 +1,6 @@
+window.xxx = (data) =>{
+  console.log(data)
+}
 const script=document.createElement('script')
 script.src='http://qq.com:8889/friends.js'
-script.onload=()=>{
-  console.log(window.xxx)
-}
 document.body.appendChild(script)
